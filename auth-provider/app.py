4964be00413209fcdcd37226d7d39a3c80e4deab
Adendorff, D, Miss [24051055@sun.ac.za] <[24051055@sun.ac.za]>
2023-10-20 06:02:57 +0200

    e

--- auth-provider/app.py
+++ auth-provider/app.py
@@ -10,3 +10,7 @@ app = create_app({
 
 if os.environ.get('INIT_DB') == "true":
     init_db()
+
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
